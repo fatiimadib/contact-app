f57fe61c7dd95c37949b3aa35a80122fc4fc51c2
fatiimadib <fatima@nahaus.de>
2023-10-05 08:43:40 +0300

    appMdoules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import{MatDialogModule} from '@angular/material/dialog'
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
 import { ContactComponent } from './components/contact/contact.component';
 import { ContactService } from './services/contact.service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { AddDialogComponent } from './components/contact/add-dialog/add-dialog.component';
 @NgModule({
   declarations: [
     AppComponent,
-    ContactComponent
+    ContactComponent,
+    AddDialogComponent
   ],
   imports: [
     BrowserModule,
@@ -20,9 +27,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatTableModule,
     BrowserAnimationsModule,
     MatIconModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatToolbarModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
-  providers: [ContactService],
+  providers: [ContactService,ContactComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
